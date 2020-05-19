397a6f61dca686f97b873feabe78d14ec0f932c7
Luan Coleto <luan@idpro.com.br>
2020-05-19 11:06:02 -0300

    Mining Type select

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,5 +1,7 @@
 class CoinsController < ApplicationController
   before_action :set_coin, only: [:show, :edit, :update, :destroy]
+  before_action :set_mining_type_options, only: [:new, :edit, :update, :create]
+
 
   # GET /coins
   # GET /coins.json
@@ -67,8 +69,12 @@ class CoinsController < ApplicationController
       @coin = Coin.find(params[:id])
     end
 
+    def set_mining_type_options
+      @mining_type_option = MiningType.all.pluck(:description, :id)
+    end
+
     # Only allow a list of trusted parameters through.
     def coin_params
-      params.require(:coin).permit(:description, :acronym, :url_image)
+      params.require(:coin).permit(:description, :acronym, :url_image, :mining_type_id)
     end
 end
