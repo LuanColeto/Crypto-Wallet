d140cddcbcd66e5a0c98617892cf43850496dc3b
Luan Coleto <luancoleto@outlook.com>
2020-05-25 10:08:56 -0300

    Webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-require("notify.js")
+require("bootstrap-notify")
 require("bootstrap")
 
-$.notify("Hello World");
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
